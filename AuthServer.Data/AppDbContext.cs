6257c1be9180e1d9dfe362d2affc2ac4619fd6f8
dogustuluk <dogus.tuluk@gmail.com>
2022-03-25 15:41:01 +0300

    appdbcontext update1

--- AuthServer.Data/AppDbContext.cs
+++ AuthServer.Data/AppDbContext.cs
@@ -12,12 +12,17 @@ namespace AuthServer.Data
 {
     public class AppDbContext: IdentityDbContext<UserApp, IdentityRole, string>
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) //options işleminin "AppDbContext" üzerinde yapılacağını belirtiyoruz,
+            //ismini ise "options" olarak veriyoruz. ":base(options)" ile base constructor'a yolluyoruz (identitydbcontext'in miras alındığı constructor'a)
         {
 
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<UserRefreshToken>  UserRefreshTokens { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
