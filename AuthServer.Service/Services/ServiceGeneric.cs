203c2a4bb0779b5a7d808b00a77786d48c60b4d0
dogustuluk <dogus.tuluk@gmail.com>
2022-03-30 12:59:36 +0300

    service generic> constructor created

--- AuthServer.Service/Services/ServiceGeneric.cs
+++ AuthServer.Service/Services/ServiceGeneric.cs
@@ -1,4 +1,6 @@
-﻿using AuthServer.Core.Services;
+﻿using AuthServer.Core.Repositories;
+using AuthServer.Core.Services;
+using AuthServer.Core.UnitOfWork;
 using SharedLibrary.Dtos;
 using System;
 using System.Collections.Generic;
@@ -11,6 +13,16 @@ namespace AuthServer.Service.Services
 {
     public class ServiceGeneric<TEntity, TDto> : IServiceGeneric<TEntity, TDto> where TEntity : class where TDto : class
     {
+        //save changes, unit of work, reporsitory'i çağıracağımız class. veri tabanı ile iletişim kurulacak yer.
+        private readonly IUnitOfWork _unitOfWork; //başında "_" olması private olduğu için bir yazım kuralıdır.
+        private readonly IGenericRepository<TEntity> _genericRepository;
+        public ServiceGeneric(IUnitOfWork unitOfWork, IGenericRepository<TEntity> genericRepository)
+        {
+            _genericRepository = genericRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+
         public Task<Response<TDto>> AddAsync(TEntity entity)
         {
             throw new NotImplementedException();
