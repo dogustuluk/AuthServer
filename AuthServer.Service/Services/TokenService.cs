bb9deacb0b647d9199d8f40a6d8b7cd4a7d95ded
dogustuluk <dogus.tuluk@gmail.com>
2022-03-30 17:41:05 +0300

    service >> TokenService created

---
+++ AuthServer.Service/Services/TokenService.cs
@@ -0,0 +1,36 @@
+﻿using AuthServer.Core.Configuration;
+using AuthServer.Core.DTOs;
+using AuthServer.Core.Entity;
+using AuthServer.Core.Services;
+using Microsoft.Extensions.Options;
+using SharedLibrary.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly UserApp _userApp;
+        private readonly CustomTokenOption _tokenOption;
+
+        public TokenService(UserApp userApp, IOptions<CustomTokenOption>options)
+        {
+            _userApp = userApp;
+            _tokenOption = options.Value;
+        }
+
+        public TokenDto CreateToken(UserApp userApp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientTokenDto CreateTokenByClient(Client client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
