1652eff05436d45ed6634c7f6db01a1df00b3160
dogustuluk <dogus.tuluk@gmail.com>
2022-04-01 13:08:23 +0300

    service > services >> AuthenticationService >>> CreateTokenAsync is created

--- AuthServer.Service/Services/AuthenticationService.cs
+++ AuthServer.Service/Services/AuthenticationService.cs
@@ -5,6 +5,7 @@ using AuthServer.Core.Repositories;
 using AuthServer.Core.Services;
 using AuthServer.Core.UnitOfWork;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using SharedLibrary.Dtos;
 using System;
@@ -19,7 +20,7 @@ namespace AuthServer.Service.Services
     {
         private readonly UserManager<UserApp> _userManager;
         private readonly List<Client> _clients;
-        private readonly IGenericRepository<UserRefreshToken> _userRefreshToken;
+        private readonly IGenericRepository<UserRefreshToken> _userRefreshTokenService;
         private readonly ITokenService _tokenService;
         private readonly IUnitOfWork _unitOfWork;
 
@@ -28,15 +29,46 @@ namespace AuthServer.Service.Services
         {
             _userManager = userManager;
             _clients = optionsClient.Value;
-            _userRefreshToken = userRefreshTokenService;
+            _userRefreshTokenService = userRefreshTokenService;
             _tokenService = tokenService;
             _unitOfWork = unitOfWork;
         }
 
 
-        public Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto)
+        public async Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto)
         {
-            throw new NotImplementedException();
+            //if'lerle tek tek kontrol etmeye "savunmacı yaklaşım" denmektedir. iç içe if-else blokları yazılmaz
+            if (loginDto == null) throw new ArgumentNullException(nameof(loginDto)); //eğer loginDto boş ise hata fırlat
+
+            var user = await _userManager.FindByEmailAsync(loginDto.Email); //kullanıcının email ini buluyoruz
+            
+            if (user == null) return Response<TokenDto>.Fail("Email or password is wrong", 400, true); //email veya şifre hatalı mesajının olmasının
+            //sebebi ise kötü niyetli kullanıcının çıkarım yapmasının önüne geçmektir.
+
+            if(!await _userManager.CheckPasswordAsync(user, loginDto.Password))
+            {
+                return Response<TokenDto>.Fail("Email or password is wrong", 400, true);
+            }
+            var token = _tokenService.CreateToken(user);
+
+            var userRefreshToken = await _userRefreshTokenService.where(x => x.UserId == user.Id).SingleOrDefaultAsync();
+
+            if (userRefreshToken == null)
+            {
+                await _userRefreshTokenService.AddAsync(new UserRefreshToken { UserId = user.Id, Code = token.RefreshToken, 
+                    Expiration = token.RefreshTokenExpiration });
+
+            }
+            else
+            {
+                userRefreshToken.Code = token.RefreshToken;
+                userRefreshToken.Expiration = token.RefreshTokenExpiration;
+            }
+
+            await _unitOfWork.CommitAsync();
+            return Response<TokenDto>.Success(token, 200);
+
+            
         }
 
         public Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto)
