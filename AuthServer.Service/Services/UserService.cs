95618e4b6bf2b83c4f6c51a0eeb9511cbb199efa
dogustuluk <dogus.tuluk@gmail.com>
2022-04-01 14:37:27 +0300

    service > services > UserService user service constructor builded

---
+++ AuthServer.Service/Services/UserService.cs
@@ -0,0 +1,31 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.Entity;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Identity;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<UserApp> _userManager;
+        public UserService(UserManager<UserApp> userManager)
+        {
+            _userManager = userManager;
+        }
+        public Task<Response<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<UserAppDto>> GetUserByNameAsync(string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
