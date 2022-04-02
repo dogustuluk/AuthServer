f44074f521faa5e5f9746966b8c9cfacbdcb59e7
dogustuluk <dogus.tuluk@gmail.com>
2022-04-02 17:18:26 +0300

    API >> appsettings > "connection strings" added, Startup.cs p1 completed

--- _AuthServer.API/Startup.cs
+++ _AuthServer.API/Startup.cs
@@ -1,8 +1,17 @@
 using AuthServer.Core.Configuration;
+using AuthServer.Core.Entity;
+using AuthServer.Core.Repositories;
+using AuthServer.Core.Services;
+using AuthServer.Core.UnitOfWork;
+using AuthServer.Data;
+using AuthServer.Data.Repositories;
+using AuthServer.Service.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +37,35 @@ namespace _AuthServer.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //DI Register kodlarý
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>)); //generic olan yapýlarda type'ýný belli ederiz.
+            services.AddScoped(typeof(IServiceGeneric<,>), typeof(ServiceGeneric<,>)); //generic içerisinde iki adet entity döndüðü için "<>"
+                                                                                       //içerisinde kaç adet entity varsa virgül konur.
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            //yukarýdaki kodlarla constructorlarda geçmiþ olduðumuz class'larý DI Container'a ekledik.
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("SqlServer"),sqlOptions =>
+                {
+                    sqlOptions.MigrationsAssembly("AuthServer.Data"); //Projenin ismi ne ise Assembly'nin ismi de o olmalýdýr.
+                }); //veri tabaný yolunu bildirmiþ oluyoruz.
+            });
+
+
+            //DI Container'a ekliyoruz
+            //Kullanýcýnýn "UserApp" olduðunu belirtiyoruz, rolü ise identity kütüphanesinden gelen default role. ek olarak rol eklemek istersek UserApp classýnda
+            //ilgili alanda açýklama satýrý halinde bir örnek mevcuttur.
+            services.AddIdentity<UserApp, IdentityRole>(Opt =>
+            {
+                Opt.User.RequireUniqueEmail = true; //Email'in veri tabanýnda benzersiz olmasý için gerekli kod.
+                Opt.Password.RequireNonAlphanumeric = false; // '*,?,-' gibi karakterlerin þifre alanýnda zorunlu olmasýný engelleyen kod.
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders(); //"AddDefaultTokenProviders" >> þifre sýfýrlama gibi iþlemlerde yeni bir token 
+            //üretmek için gerekiyor.
+
+
             services.Configure<CustomTokenOption>(Configuration.GetSection("TokenOption"));
             services.Configure<List<Client>>(Configuration.GetSection("Clients"));
 
