5a2276dcf189f7674d0f49c260a4108b2cc86e2d
dogustuluk <dogus.tuluk@gmail.com>
2022-04-06 13:52:16 +0300

    Api> AuthController has been created

---
+++ _AuthServer.API/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _AuthServer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+        public AuthController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+
+    }
+}
