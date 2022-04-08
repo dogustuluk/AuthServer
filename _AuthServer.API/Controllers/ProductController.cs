63f881483c0145fb5ad38840fca19a58baa6408a
dogustuluk <dogus.tuluk@gmail.com>
2022-04-08 17:14:27 +0300

    Api > ProductController has been created

---
+++ _AuthServer.API/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.Entity;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _AuthServer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IServiceGeneric<Product, ProductDto> _productService;
+
+        public ProductController(IServiceGeneric<Product, ProductDto> productService)
+        {
+            _productService = productService;
+        }
+    }
+}
