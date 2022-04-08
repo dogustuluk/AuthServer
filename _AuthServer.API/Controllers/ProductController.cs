d0fda8d0906c3a858c9fff9af4f7efaa9ce7b0a5
dogustuluk <dogus.tuluk@gmail.com>
2022-04-08 17:22:55 +0300

    api > Product controller's crud operations has been created

--- _AuthServer.API/Controllers/ProductController.cs
+++ _AuthServer.API/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using AuthServer.Core.DTOs;
 using AuthServer.Core.Entity;
 using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -10,15 +11,39 @@ using System.Threading.Tasks;
 
 namespace _AuthServer.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductController : ControllerBase
+    public class ProductController : CustomBaseController
     {
         private readonly IServiceGeneric<Product, ProductDto> _productService;
 
         public ProductController(IServiceGeneric<Product, ProductDto> productService)
         {
             _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProduct()
+        {
+            return ActionResultInstance(await _productService.GetAllAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveProduct(ProductDto productDto)
+        {
+            return ActionResultInstance(await _productService.AddAsync(productDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(ProductDto productDto)
+        {
+            return ActionResultInstance(await _productService.Update(productDto, productDto.Id));
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            return ActionResultInstance(await _productService.Remove(id));
         }
     }
 }
