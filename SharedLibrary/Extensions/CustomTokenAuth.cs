7725594fdf78eec61271a5ac9c049cc1e3bcb5de
dogustuluk <dogus.tuluk@gmail.com>
2022-04-08 20:08:30 +0300

    CustomTokenAuth > AddCustomTokenAuth has been created and signService moved to SharedLibrary's services layer

--- SharedLibrary/Extensions/CustomTokenAuth.cs
+++ SharedLibrary/Extensions/CustomTokenAuth.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using SharedLibrary.Configurations;
+using SharedLibrary.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,33 @@ namespace SharedLibrary.Extensions
 {
     public static class CustomTokenAuth
     {
+        public static void AddCustomTokenAuth(this IServiceCollection services, CustomTokenOption tokenOptions)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; //bu kod satırı; üstteki kod satırı ile alttaki kod satırının birbiriyle iletişim
+                //halinde olması sağlamaktadır.
+                //3'ünün de ismi aynı olmalıdır ki birbirleriyle iletişim halinde olabilsinler.
+            }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opts =>
+            {
+                opts.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    ValidIssuer = tokenOptions.Issuer,
+                    ValidAudience = tokenOptions.Audience[0], //audience'ların ilk indisini vermemizin sebebi bizim ana projemiz olmasıdır. ana projeyi vererek diğer
+                    //audience'lara da erişim sağlatabiliriz.
+                    IssuerSigningKey = SignService.GetSymmetricSecurityKey(tokenOptions.SecurityKey),
+
+                    //kontrol ediliyor
+                    ValidateIssuerSigningKey = true,
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero //default olarak verilen 5dk'yı sıfırlar. farklı serverlarda çalışmaktan dolayı oluşan zamanlama hatalarını tolere edebilmek
+                    //için default değer 5dk'dır. test amaçlı olması nedeniyle bunu 0'a çekeriz.
+                    //.
+                };
+            });
+        }
     }
 }
