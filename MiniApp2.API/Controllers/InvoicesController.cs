812b4101f28d7cbddb38b0973b597ff4b054dffd
dogustuluk <dogus.tuluk@gmail.com>
2022-04-29 13:56:06 +0300

    miniapi2 invoiceController has been created

---
+++ MiniApp2.API/Controllers/InvoicesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MiniApp2.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoicesController : ControllerBase
+    {
+        public IActionResult GetInvoices()
+        {
+            var userName = HttpContext.User.Identity.Name;
+            var userMail = User.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Email);
+            var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+            return Ok($"Invoice Transactions => userId: {userId} - userName: {userName} - userMail: {userMail}");
+        }
+    }
+}
