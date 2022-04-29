322d177d56e8d5247d8eb9cf5dcfaefea938c31e
dogustuluk <dogus.tuluk@gmail.com>
2022-04-29 13:44:34 +0300

    miniapi1 StockController has been created

---
+++ MiniApp1.API/Controllers/StockController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MiniApp1.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        public IActionResult GetStock()
+        {
+            var userName = HttpContext.User.Identity.Name; //endpoint'e istek yapıldığında token'ın payload'ından name verisini çeker.
+            //yukarıdaki name ile veri tabanından istenilen kullanıcıya ait verileri çekebiliriz.
+            var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+            //veri tabanından userName veya userId alanları üzerinden gerekli dataları çekebiliriz.
+            var userMail = User.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Email); //token'dan mail adresini almak için.
+            //bu verileri çekerken ezbere gitmemek için TokenService'teki ilgili metot içerisinden (genellikle GetClaims olmakta) alınır.
+            return Ok($"UserName: {userName} - UserId: {userId} - UserMail: {userMail}");
+
+        }
+
+    }
+}
